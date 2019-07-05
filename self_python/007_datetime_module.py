0f264d70733664b847fb880f23cf40df97fc7ecc
hq2nguye <ng.hieu100@gmail.com>
2019-07-05 07:13:53 -0400

    july 5

--- self_python/007_datetime_module.py
+++ self_python/007_datetime_module.py
@@ -2,17 +2,19 @@
 import datetime
 import pytz
 
-# Naive
+# Naive : y, m, d
 # d = datetime.date(2001, 9, 11)
 
 tday = datetime.date.today()
 
 
-# weekday() - Monday is 0 and Sunday is 6
-# print(tday)
+# weekday() # - Monday is 0 and Sunday is 6
+print(tday)
+print(tday.weekday())
+
 
 # isoweekday() - Monday is 1 and Sunday is 7
-# print(tday)
+print(tday.isoweekday())
 
 
 # datetime.timedelta(days=0, seconds=0, microseconds=0, milliseconds=0, minutes=0, hours=0, weeks=0)

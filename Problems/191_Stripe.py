4cba7c43c3964b215530acccae2f25ce34cb772e
hq2nguye <ng.hieu100@gmail.com>
2019-09-07 11:17:33 -0400

    September 7th 2019 205 problems finalized

--- Problems/191_Stripe.py
+++ Problems/191_Stripe.py
@@ -9,3 +9,22 @@ For example, given the intervals (7, 9), (2, 4), (5, 8), return 1 as the last in
 
 The intervals are not necessarily sorted in any order.
 """
+
+"""
+We can do this problem greedily. If we sort the intervals by the end time, and continuously choose the first interval that ends first, we'll minimize the number of overlapping intervals with that one. We can compute overlapping intervals quickly by just keeping track of the end of the last interval we haven't overlapped with:
+"""
+
+from math import inf
+
+def non_overlapping_intervals(intervals):
+    current_end = -inf
+    overlapping = 0
+
+    for start, end in sorted(intervals, key=lambda i: i[1]):
+        if start >= current_end:
+            current_end = end
+        else:
+            overlapping += 1
+    return overlapping
+
+# This takes O(n log n) time, since we have to sort the intervals. It also takes O(1) time.

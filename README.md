53fdfa2e45ec03eaeb4e538cc3901314fafda5d4
hq2nguye <ng.hieu100@gmail.com>
2020-02-01 17:29:40 -0500

    completed getting all the data as required, next is to parse the each subsection accordingly

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 ## Setup anaconda env note:
 https://uoa-eresearch.github.io/eresearch-cookbook/recipe/2014/11/20/conda/
+
+1. test if a module has been install successully: python -c "import module"
--- self_python/WebScrapingAutomate/002_Selenium.py
+++ self_python/WebScrapingAutomate/002_Selenium.py
@@ -6,6 +6,14 @@ then move this driver to the same dir that selenium will look for
 also need to:
 - cd Downloads
 - cp geckodriver /Users/hq2nguye/geckodriver
+
+OR ON MAC:
+
+brew install geckodriver
+
+which geckodiver : to get the path
+
+driver = webdriver.Firefox(executable_path = '/usr/local/bin/geckodriver')
 """
 
 """
@@ -41,14 +49,15 @@ from selenium import webdriver
 # the only thing that chages is the url
 
 # tells selenmn where chrome driver is stored
-chromedriver = "/home/captainhampton/Downloads/chromedriver"
-driver = webdriver.Chrome(chromedriver)
-driver.get("http:google.com")
+# chromedriver = "/Users/hq2nguye/Downloads/chromedriver"
+# driver = webdriver.Chrome(chromedriver)
+# driver.get("http:google.com")
 
 # set max page number: how many times are we gonna click the "load more"
-MAX_PAGE_NUM = 5
+MAX_PAGE_NUM = 1  # 43
 # set max page digit: this is for editting the url
 MAX_PAGE_DIG = 3
+# Note we nolong need to specify a specific webpage here
 driver = webdriver.Firefox()
 
 with open('results.csv', 'w') as f:
@@ -60,17 +69,38 @@ for i in range(1, MAX_PAGE_NUM + 1):
     + padding string i with appropirate 0 in front: * "0"
     """
     page_num = (MAX_PAGE_DIG - len(str(i))) * "0" + str(i)
-    url = "http://econpy.pythonanywhere.com/ex/" + page_num + ".html"
+    url = "https://insolvencyinsider.ca/filing/?fwp_load_more=" + page_num
+    # url = "http://econpy.pythonanywhere.com/ex/" + page_num + ".html"
 
     driver.get(url)
+    test_filing_entry = driver.find_elements_by_xpath('//div[@class="filing-entry"]')
+    # test_filing_head = driver.find_elements_by_xpath('//div[@class="filing-head"]')
+    # test_filing_meta = driver.find_elements_by_xpath('//div[@class="filing-meta"]')
 
-    buyers = driver.find_elements_by_xpath('//div[@title="buyer-name"]')
-    prices = driver.find_elements_by_xpath('//span[@class="item-price"]')
+    # test_label = driver.find_elements_by_xpath('//span[@class="filing-meta-label"]')
 
-    num_page_items = len(buyers)
+    # test_content = driver.find_elements_by_xpath('//div[@class="filing-content"]')
+    print(len(test_filing_entry))
+    # print(test_label)
+    for e in test_filing_entry:
+        print(len([i for i in e.text.split("\n")]))
+        print(e.text)
+        print("\n")
+
+    print([i for i in test_filing_entry[0].text.split("\n")])
+    # num_page_items = len(test_label)
     # write to a csv files
-    with open('results.csv', 'a') as f:
-        for i in range(num_page_items):
-            f.write(buyers[i].text + "," + prices[i].text + "\n")
+    # with open('results.csv', 'a') as f:
+    #     for i in range(num_page_items):
+    #         f.write(test_label[i].text + "," + class_content[i].text + "\n")
+
+    # filing_type = driver.find_elements_by_xpath('//div[@title="buyer-name"]')
+    # prices = driver.find_elements_by_xpath('//span[@class="item-price"]')
+
+    # num_page_items = len(buyers)
+    # write to a csv files
+    # with open('results.csv', 'a') as f:
+    #     for i in range(num_page_items):
+    #         f.write(buyers[i].text + "," + prices[i].text + "\n")
 
 driver.close()
